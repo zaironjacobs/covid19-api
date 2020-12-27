6eb9bedf42e46c9ede9adb6f0c8be4082e87442f
Zairon Jacobs <zaijacobs@gmail.com>
2020-12-27 17:08:57 +0100

    http exception added

--- covid19_api/app.py
+++ covid19_api/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from .mongo_database import MongoDatabase
 from .country_helper import parse_country
@@ -10,6 +10,8 @@ mongo_database = MongoDatabase()
 @app.get('/country/', tags=["country"])
 async def get_country(name: str):
     country = await mongo_database.get_country(name)
+    if not country:
+        raise HTTPException(status_code=404, detail="Country not found")
     return parse_country(country)
 
 
@@ -19,4 +21,6 @@ async def get_countries():
     countries = mongo_database.get_all_countries()
     async for country in countries:
         results.append(parse_country(country))
+    if not results:
+        raise HTTPException(status_code=404, detail="Countries not found")
     return results
