734b7b6ca20982122994dca6a701161896481a65
Zairon Jacobs <zaijacobs@gmail.com>
2022-12-16 09:43:37 +0100

    remove extra fields

--- covid19_api/app.py
+++ covid19_api/app.py
@@ -26,8 +26,6 @@ async def get_country(country_name: str):
             name=country.get("name"),
             confirmed=country.get("confirmed"),
             deaths=country.get("deaths"),
-            active=country.get("active"),
-            recovered=country.get("recovered"),
             last_updated_by_source_at=country.get("last_updated_by_source_at"),
         )
         return country.dict()
@@ -46,8 +44,6 @@ async def get_countries():
             name=country.get("name"),
             confirmed=country.get("confirmed"),
             deaths=country.get("deaths"),
-            active=country.get("active"),
-            recovered=country.get("recovered"),
             last_updated_by_source_at=country.get("last_updated_by_source_at"),
         )
         results.append(country.dict())
